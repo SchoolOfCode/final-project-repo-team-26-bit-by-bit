e93839c3bab2ade59d116825f2e420d65f2b29f0
UmkalthumS <93615375+UmkalthumS@users.noreply.github.com>
2022-02-22 22:30:49 +0000

    Add tables for custom sections

--- db/scripts/users/createTable.js
+++ db/scripts/users/createTable.js
@@ -47,14 +47,28 @@ CREATE TABLE IF NOT EXISTS customise(
   isSimple boolean
 );
 
+CREATE TABLE IF NOT EXISTS custom_section(
+  id int,
+  user_id int,
+  name varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS custom_section_item(
+  id int,
+  section_id int,
+  text varchar(255),
+  due_date DATE
+);
+
 ALTER TABLE todo_list ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
 
 ALTER TABLE reminder_list ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
 
 ALTER TABLE goals ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
 
-ALTER TABLE customise ADD FOREIGN KEY (user_id) REFERENCES users (user_id);`
+ALTER TABLE customise ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
 
+ALTER TABLE custom_section_item ADD FOREIGN KEY (section_id) REFERENCES section (id);`
 
 async function createTable (){
 
