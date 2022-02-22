f0a037989eccda0ec3b2b9e04783f67a41d927b4
UmkalthumS <93615375+UmkalthumS@users.noreply.github.com>
2022-02-22 22:39:10 +0000

    Refactor ids for tables

--- db/scripts/users/createTable.js
+++ db/scripts/users/createTable.js
@@ -1,14 +1,14 @@
 import query from "../../connection.js";
 
 const sqlString = `CREATE TABLE IF NOT EXISTS users(
-  user_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   full_name varchar(255),
   created timestamp not null default CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS todo_list(
+  id int,
   user_id int,
-  todo_id int,
   text varchar(255),
   priority varchar(255),
   created date not null default CURRENT_DATE,
@@ -19,16 +19,16 @@ CREATE TABLE IF NOT EXISTS todo_list(
 );
 
 CREATE TABLE IF NOT EXISTS reminder_list(
+  id int, 
   user_id int, 
-  reminder_id int, 
   text varchar(255),
   due_date DATE, 
   isCompleted boolean 
 );
 
 CREATE TABLE IF NOT EXISTS goals (
+  id int ,
   user_id int,
-  goals_id int ,
   text varchar(255),
   priority varchar(255),
   created date not null default CURRENT_DATE,
@@ -50,13 +50,13 @@ CREATE TABLE IF NOT EXISTS custom_section_item(
   due_date DATE
 );
 
-ALTER TABLE todo_list ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
+ALTER TABLE todo_list ADD FOREIGN KEY (user_id) REFERENCES users (id);
 
-ALTER TABLE reminder_list ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
+ALTER TABLE reminder_list ADD FOREIGN KEY (user_id) REFERENCES users (id);
 
-ALTER TABLE goals ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
+ALTER TABLE goals ADD FOREIGN KEY (user_id) REFERENCES users (id);
 
-ALTER TABLE customise ADD FOREIGN KEY (user_id) REFERENCES users (user_id);
+ALTER TABLE customise ADD FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE custom_section_item ADD FOREIGN KEY (section_id) REFERENCES section (id);`
 
