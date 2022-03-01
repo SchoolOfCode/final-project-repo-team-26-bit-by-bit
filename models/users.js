c76c1f60472444c1477ea24e01ea41841229402f
Katherine Tweeddale <Katweeddale@gmail.com>
2022-03-01 05:55:52 +0000

    on conflict

--- models/users.js
+++ models/users.js
@@ -15,7 +15,7 @@ export async function createUser(body) {
   const user_id = body.user_id;
   const full_name = body.full_name;
   const data = await query(
-    `INSERT INTO users( user_id, full_name) VALUES ($1, $2) RETURNING *;`,
+    `INSERT INTO users( user_id, full_name) VALUES ($1, $2) ON CONFLICT DO NOTHING RETURNING *;`,
     [user_id, full_name]
   );
   return data.rows;
