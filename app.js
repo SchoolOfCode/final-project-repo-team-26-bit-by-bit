e44ff42cf9a8bdc7aee0cadcba18ff2a5430fb62
Katherine Tweeddale <Katweeddale@gmail.com>
2022-03-01 01:41:12 +0000

    update

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-import express from 'express';
-import path from 'path';
-import __dirname  from './dirname.js';
-import cookieParser  from 'cookie-parser';
-import cors  from 'cors';
-import logger  from 'morgan';
-import createError from 'http-errors'
-import usersRouter  from './routes/users.js';
-
-//export default function(database) {
+import express from "express";
+import path from "path";
+import __dirname from "./dirname.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import logger from "morgan";
+import createError from "http-errors";
+import usersRouter from "./routes/users.js";
 
 const app = express();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 app.use(async function (req, res) {
-  res.status(404).json({message: "We couldn't find what you were looking for 😞"})
-})
+  res
+    .status(404)
+    .json({ message: "We couldn't find what you were looking for 😞" });
+});
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).json(err)
-})
+  console.error(err.stack);
+  res.status(500).json(err);
+});
 //return app}
 
 export default app;
