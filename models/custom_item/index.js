e7b2f46aeda388117b36b44b35f6bc5f7aa09f33
Katherine Tweeddale <Katweeddale@gmail.com>
2022-03-03 11:05:28 +0000

    buttons

--- models/custom_item/index.js
+++ models/custom_item/index.js
@@ -1,12 +1,13 @@
 import query from "../../db/connection.js";
 
 export async function getCustomSectionItemByUsers(user_id) {
-  const result = await query(`SELECT * FROM custom_section_item WHERE user_id = $1;`, [
-    user_id,
-  ]);
+  const result = await query(
+    `SELECT * FROM custom_section_item WHERE user_id = $1;`,
+    [user_id]
+  );
   return result.rows;
 }
-//
+////
 export async function getCustomSectionItemByID(section_id, user_id) {
   const result = await query(
     `SELECT * FROM custom_section_item WHERE section_id = $1 AND user_id = $2;`,
@@ -31,17 +32,13 @@ export async function createCustomSectionItem(body) {
 
 export async function updateCustomSectionItem(body) {
   const user_id = body.user_id;
- const text = body.text;
- const due_date = body.due_date;
+  const text = body.text;
+  const due_date = body.due_date;
 
- const section_id = body.section_id;
+  const section_id = body.section_id;
   const data = await query(
     `UPDATE custom_section_item SET user_id = $1, text=$2, due_date=$3 WHERE section_id = $4 RETURNING *;`,
-    [user_id,
-        text,
-        due_date,
-        section_id ]
-
+    [user_id, text, due_date, section_id]
   );
 
   return data.rows;
