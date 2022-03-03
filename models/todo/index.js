997ba20654cb51ed1bf60188592de1f0c6d7b948
Katherine Tweeddale <Katweeddale@gmail.com>
2022-03-03 14:02:06 +0000

    update

--- models/todo/index.js
+++ models/todo/index.js
@@ -24,18 +24,32 @@ export async function createToDoList(body) {
   const text = body.text;
   const time = body.time;
   const priority = body.priority;
-  const isMonday = body.isMonday;
-  const isTuesday = body.isTuesday;
-  const isWednesday = body.isWednesday;
-  const isThursday = body.isThursday;
-  const isFriday = body.isFriday;
-  const isSaturday = body.isSaturday;
-  const isSunday = body.isSunday;
+  const ismonday = body.ismonday;
+  const istuesday = body.istuesday;
+  const iswednesday = body.iswednesday;
+  const isthursday = body.isthursday;
+  const isfriday = body.isfriday;
+  const issaturday = body.issaturday;
+  const issunday = body.issunday;
   const iscompleted = body.iscompleted;
 
   const data = await query(
     `INSERT INTO todo_list (user_id, todo_id, text, priority, time, isMonday, isTuesday, isWednesday, isThursday, isFriday, isSaturday, isSunday, isCompleted) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *;`,
-    [user_id, todo_id, text, priority, time, isMonday, isTuesday, isWednesday, isThursday, isFriday, isSaturday, isSunday, iscompleted]
+    [
+      user_id,
+      todo_id,
+      text,
+      priority,
+      time,
+      ismonday,
+      istuesday,
+      iswednesday,
+      isthursday,
+      isfriday,
+      issaturday,
+      issunday,
+      iscompleted,
+    ]
   );
   return data.rows;
 }
@@ -45,18 +59,32 @@ export async function updateToDo(body) {
   const text = body.text;
   const priority = body.priority;
   const time = body.time;
-  const isMonday = body.isMonday;
-  const isTuesday = body.isTuesday;
-  const isWednesday = body.isWednesday;
-  const isThursday = body.isThursday;
-  const isFriday = body.isFriday;
-  const isSaturday = body.isSaturday;
-  const isSunday = body.isSunday;
+  const ismonday = body.ismonday;
+  const istuesday = body.istuesday;
+  const iswednesday = body.iswednesday;
+  const isthursday = body.isthursday;
+  const isfriday = body.isfriday;
+  const issaturday = body.issaturday;
+  const issunday = body.issunday;
   const iscompleted = body.iscompleted;
   const todo_id = body.todo_id;
   const data = await query(
     `UPDATE todo_list SET user_id = $1, text=$2, priority=$3, time=$4, isMonday=$5, isTuesday=$6, isWednesday=$7, isThursday=$8, isFriday=$9, isSaturday=$10, isSunday=$11, isCompleted=$12,  WHERE todo_id = $13 RETURNING *;`,
-    [user_id, text, priority, time, isMonday, isTuesday, isWednesday, isThursday, isFriday, isSaturday, isSunday, iscompleted, todo_id]
+    [
+      user_id,
+      text,
+      priority,
+      time,
+      ismonday,
+      istuesday,
+      iswednesday,
+      isthursday,
+      isfriday,
+      issaturday,
+      issunday,
+      iscompleted,
+      todo_id,
+    ]
   );
   return data.rows;
 }
